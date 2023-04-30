fe8825e05b6a99f2f97b66ad5479daae8b2cbaa2
Conner <clramirezxr1999@gmail.com>
2023-04-29 23:21:03 -0700

    Music Service

--- app/src/main/java/com/example/concentrationgame/MainActivity.java
+++ app/src/main/java/com/example/concentrationgame/MainActivity.java
@@ -1,17 +1,23 @@
 package com.example.concentrationgame;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.annotation.SuppressLint;
-import android.app.Dialog;
-import android.content.Intent;
-import android.content.SharedPreferences;
-import android.media.MediaPlayer;
-import android.os.Bundle;
-import android.widget.Button;
 import androidx.appcompat.widget.SwitchCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import android.annotation.SuppressLint;
+import android.app.ActivityManager;
+import android.app.Dialog;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.content.SharedPreferences;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.view.View;
+import android.widget.Button;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -21,67 +27,81 @@ import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button playBtn;
-    Button leaderBoardBtn;
-    Button settingsBtn;
-    SwitchCompat onOffSwitch;
-
-    public SharedPreferences preferences;
-    public SharedPreferences.Editor editor;
-    public MediaPlayer music;
+    private Button playBtn;
+    private Button leaderBoardBtn;
+    private Button settingsBtn;
+    private SwitchCompat onOffSwitch;
+    private MediaPlayer music;
+    private MusicService musicService;
+    private boolean isServiceRunning;
+    private SharedPreferences preferences;
+    private SharedPreferences.Editor editor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Intent musicServiceIntent = new Intent(this, MusicService.class);
+        bindService(musicServiceIntent, musicServiceConnection, Context.BIND_AUTO_CREATE);
+
+        isServiceRunning = true;
         preferences = this.getSharedPreferences("MUSIC", 0);
         editor = preferences.edit();
 
-        music = MediaPlayer.create(this, R.raw.bg_music);
-        music.setLooping(true);
-        music.start();
-
         playBtn = findViewById(R.id.playBtn);
-
         playBtn.setOnClickListener(view -> {
             Intent intent = new Intent(MainActivity.this, GameModeActivity.class);
             startActivity(intent);
         });
 
         leaderBoardBtn = findViewById(R.id.leaderBoardBtn);
-
         leaderBoardBtn.setOnClickListener(view -> showLeaderDialog());
 
         settingsBtn = findViewById(R.id.settingsBtn);
-
         settingsBtn.setOnClickListener(view -> showSettingsDialog());
     }
 
-    protected void showLeaderDialog()
-    {
+    private ServiceConnection musicServiceConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
+            MusicService.LocalBinder binder = (MusicService.LocalBinder) iBinder;
+            musicService = binder.getService();
+            boolean isMusicOn = preferences.getBoolean("ON", true);
+            if (isMusicOn) {
+                musicService.startMusic();
+            }
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName componentName) {
+            musicService = null;
+        }
+    };
+
+    protected void showLeaderDialog() {
         Dialog leaderDialog = new Dialog(this, R.style.DialogStyle);
         leaderDialog.setContentView(R.layout.layout_leaderboard_dialog);
         RecyclerView recyclerView = leaderDialog.findViewById(R.id.recycler);
-        List<LeaderboardItem> items = new ArrayList<LeaderboardItem>();
+        List<LeaderboardItem> items = new ArrayList<>();
 
         SharedPreferences sharedPrefs = getSharedPreferences("scores", MODE_PRIVATE);
         Map<String, ?> scores = sharedPrefs.getAll();
-        for(Map.Entry<String,?> entry: scores.entrySet()){
+        for (Map.Entry<String, ?> entry : scores.entrySet()) {
             String name = entry.getKey();
             Object score = entry.getValue();
-            items.add(new LeaderboardItem(name, (Integer)score));
+            items.add(new LeaderboardItem(name, (Integer) score));
         }
 
         items.add(new LeaderboardItem("Jeremy", 7));
 
         //sorts leaderboard by highest score
-        Collections.sort(items, new Comparator<LeaderboardItem>(){
+        Collections.sort(items, new Comparator<LeaderboardItem>() {
             @Override
-            public int compare(LeaderboardItem item2, LeaderboardItem item1){
-                if(item1.getScore() < item2.getScore())
+            public int compare(LeaderboardItem item2, LeaderboardItem item1) {
+                if (item1.getScore() < item2.getScore())
                     return -1;
-                else if(item1.getScore() == item2.getScore())
+                else if (item1.getScore() == item2.getScore())
                     return 0;
                 else
                     return 1;
@@ -96,8 +116,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @SuppressLint("UseSwitchCompatOrMaterialCode")
-    protected void showSettingsDialog()
-    {
+    protected void showSettingsDialog() {
         Dialog settingsDialog = new Dialog(this, R.style.DialogStyle);
         settingsDialog.setContentView(R.layout.layout_settings_dialog);
         settingsDialog.show();
@@ -105,27 +124,26 @@ public class MainActivity extends AppCompatActivity {
 
         onOffSwitch = settingsDialog.findViewById(R.id.onOffSwitch);
 
-        if(onOffSwitch != null)
-        {
+        if (onOffSwitch != null) {
             onOffSwitch.setChecked(true);
         }
         onOffSwitch.setOnCheckedChangeListener((compoundButton, isChecked) -> {
             if (isChecked) {
-                music.start();
+                musicService.startMusic();
                 editor.putBoolean("ON", true).commit();
             } else {
-                music.pause();
+                musicService.stopMusic();
                 editor.putBoolean("ON", false).commit();
             }
         });
 
-        // Start or pause music based on the switch state
+
         boolean isMusicOn = preferences.getBoolean("ON", true);
         onOffSwitch.setChecked(isMusicOn);
         if (isMusicOn) {
-            music.start();
+            musicService.startMusic();
         } else {
-            music.pause();
+            musicService.stopMusic();
         }
     }
 }
---
+++ app/src/main/java/com/example/concentrationgame/MusicService.java
@@ -0,0 +1,65 @@
+package com.example.concentrationgame;
+
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Binder;
+import android.os.IBinder;
+import android.widget.Toast;
+import androidx.annotation.Nullable;
+
+import com.example.concentrationgame.R;
+
+public class MusicService extends Service {
+
+    private MediaPlayer musicPlayer;
+
+    public class LocalBinder extends Binder {
+        public MusicService getService() {
+            return MusicService.this;
+        }
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // Create and start the MediaPlayer object
+        musicPlayer = MediaPlayer.create(this, R.raw.bg_music);
+        musicPlayer.setLooping(true);
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        // Return START_NOT_STICKY to indicate that the service should not be restarted if it is killed
+        return START_NOT_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+// stop music when app is closed or destroyed
+        if (musicPlayer != null) {
+            musicPlayer.stop();
+            musicPlayer.release();
+        }
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return new LocalBinder();
+    }
+
+    public void startMusic() {
+        if (musicPlayer != null && !musicPlayer.isPlaying()) {
+            musicPlayer.start();
+        }
+    }
+
+    public void stopMusic() {
+        if (musicPlayer != null && musicPlayer.isPlaying()) {
+            musicPlayer.pause();
+        }
+    }
+}
